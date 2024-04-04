65ccbd3075e7d9639c770f695c141a578d6ca5ce
buddergod311 <104363889+buddergod311@users.noreply.github.com>
2024-04-03 21:16:21 -0400

    Title component

---
+++ src/app/components/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <p>
+            Interactive schedule builder and goal management system.
+        </p>
+    );
+};
+
+export default Title;
--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,13 @@ import { useState } from 'react';
 import UsersList from './components/GoalList';
 import AddUser from './components/AddGoal';
 import Hdr from './components/Hdr';
+import Title from './components/Title';
 
 function Home() {
   return (
     <div>
       <Hdr>Goal Getter</Hdr>
+      <Title />
     </div>
   );
 }
